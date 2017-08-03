07ec152b70c41fc8dadd786031992cad1ecd83c1
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-08-03 15:30:21 -0300

    Added Grails Packages (purchase-api) (products-api) and gitignore

---
+++ products-api/grails-app/init/products/api/BootStrap.groovy
@@ -0,0 +1,9 @@
+package products.api
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ purchase-api/grails-app/init/purchase/api/BootStrap.groovy
@@ -0,0 +1,9 @@
+package purchase.api
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
