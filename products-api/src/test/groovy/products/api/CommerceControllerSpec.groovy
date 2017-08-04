2a8d9011e67c2e55b295d78c6571befe1f1d60e8
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-08-04 02:24:15 -0300

    Removed CommerceController Test

--- products-api/src/test/groovy/products/api/CommerceControllerSpec.groovy
+++
@@ -1,6 +0,0 @@
-package products.api
-
-import grails.testing.web.controllers.ControllerUnitTest
-import spock.lang.Specification
-
-class CommerceControllerSpec extends Specification implements ControllerUnitTest<CommerceController> {}
