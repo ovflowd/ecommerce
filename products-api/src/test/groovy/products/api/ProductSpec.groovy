42b4e60ffccd257c0fc1bd4386ed5a55a44b9cc3
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-08-04 23:11:26 -0300

    Coded Unit Test Case for Products, other attempt to fix Codecov (we don't know exactly which path the folder it's moved

---
+++ products-api/src/test/groovy/products/api/ProductSpec.groovy
@@ -0,0 +1,23 @@
+package products.api
+
+import grails.test.mixin.TestFor
+import spock.lang.*
+
+@TestFor(Product)
+class ProductSpec extends Specification {
+    def 'test if a normal product passes on the validation'() {
+        when:
+        def product = new Product(name: 'A normal sad product.', price: 2.00).save()
+
+        then:
+        product
+    }
+
+    def 'test if validation works when the product is invalid (price)'() {
+        when:
+        def product = new Product(price: -20, name: 'An abnormal product.').save()
+
+        then:
+        product.hasErrors()
+    }
+}
