3dbb7eb94b51bba24265f17bfc9f05e6215d66b3
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-11-28 07:42:44 -0200

    Finished v2

---
+++ purchase-api/src/test/groovy/purchase/api/LogDomainSpec.groovy
@@ -0,0 +1,14 @@
+package purchase.api
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(Log)
+class LogDomainSpec extends Specification {
+    void 'test if a normal log passes on the validation'() {
+        when:
+            def log = new Log(relatedElement: 'some-element', relatedTable: 'product', details: 'Some shit happened here.').save()
+        then:
+            log
+    }
+}
